0c3aaf82695be592bdf5aac6d21f37ab79951162
gardan4 <marcmeijers01@gmail.com>
2024-06-20 16:39:34 +0200

    working code now

--- Algorithms/clarke_wright.py
+++ Algorithms/clarke_wright.py
@@ -212,8 +212,8 @@ class ClarkeWright:
 
 if __name__ == "__main__":
     clients = []
-    clients.append(Client('Wade White', 'Geldrop', ['2024-06-06_evening']))
-    clients.append(Client('Wade Whasdite', 'Geldrop', ['2024-06-06_evening']))
+    clients.append(Client('Wade White', 'Asten Heusden Ommel', ['2024-06-06_evening']))
+    clients.append(Client('Wade Whasdite', 'Asten Heusden Ommel', ['2024-06-06_evening']))
     # clients.append(Client('Wade xc', 'Someren', ['2024-06-02_morning']))
     # clients.append(Client('b', 'Helmond', ['2024-05-16_morning', '2024-05-16_evening']))
     # clients.append(Client('c', 'Someren', ['2024-05-16_morning']))
